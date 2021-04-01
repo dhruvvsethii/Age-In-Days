7e7e37b3c18e849dc61e23a774c2d6a6a8f6d99e
Dhruv <dhruv@MacBook-Pro.local>
2021-04-01 11:34:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function ageindays() {
+    var birthyear = prompt("What year were you born in ?");
+    var age = (2021 - birthyear)*365;
+    var h1 = document.createElement('h1');
+    var answer = document.createTextNode('You are ' + age + ' days old.');
+    h1.setAttribute('id', 'age');
+    h1.appendChild(answer);
+    document.getElementById('result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('age').remove();
+}
